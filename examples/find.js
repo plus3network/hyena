6de0be2bbc47faf61589076da032befc46c83f38
Chris Cowan <chriscowan@plus3network.com>
2013-08-06 13:07:39 -0700

    Adding the ability to self referencing joins

--- examples/find.js
+++ examples/find.js
@@ -13,6 +13,7 @@ var userSchema = new Schema({
   name: { type: 'string', required: true },
   email: { type: 'email', requried: true },
   clubhouse: { type: 'Clubhouse', field: 'causes_sponsors_id', required: true },
+  bestFriend: { type: 'User', field: 'best_friend_id' }
 });
 
 var clubhouseSchema = new Schema({
@@ -20,7 +21,8 @@ var clubhouseSchema = new Schema({
   sponsor: { type: 'Sponsor', field: 'sponsor_id', required: true },
   is_wellness: { type: 'boolean', default: false },
   is_public: { type: 'boolean', default: true },
-  is_open: { type: 'boolean', default: false }
+  is_open: { type: 'boolean', default: false },
+  author: { type: 'User', field: 'user_id' }
 });
 
 var sponsorSchema = new Schema({
@@ -38,7 +40,10 @@ var Clubhouse = hyena.model('Clubhouse', clubhouseSchema, 'causes_sponsors');
 var Sponsor = hyena.model('Sponsor', sponsorSchema);
 var Cause = hyena.model('Cause', causeSchema);
 
-User.find({}, null, ['clubhouse'], function (err, docs) {
+User.find().populate('clubhouse').exec(function (err, docs) {
+  if (err) {
+    console.log(err.stack);
+  }
   console.log(JSON.stringify(docs));
   process.exit();
 });
